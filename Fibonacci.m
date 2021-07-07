129771f1cd969f20716a1fa0b17914fbed7f07e0
Shail <shailbala2@gmail.com>
2021-07-07 22:25:50 +0530

    Update Fibonacci.m

--- Fibonacci.m
+++ Fibonacci.m
@@ -1,18 +1,21 @@
-a = [25, 45, 6, 10, 0, 5, 20, 7, 41, 9];
-big = zeros(size(a));
-small = zeros(size(a));
+% fibonacci
+n=input("Number of Fibonacci Numbers to print: ");
+a = 0;
+b = 1;
 
-for i=1:size(a,2)
-    b = 0;
-    s = 0;
-    for j=1:size(a,2)
-        if a(j) > a(i)
-            b = b + 1;
-        elseif a(j) < a(i)
-            s = s + 1;
+% n should be >= 0
+if n <= 0
+    disp('Incorrect input');
+else if n == 1
+        disp(a);
+    else
+        disp(a);
+        disp(b);
+        for i=3:n
+            c = a + b;
+            a = b;
+            b = c;
+            disp(c);
         end
     end
-    big(i) = b;
-    small(i) = s;
 end
-c = [a;small;big]
